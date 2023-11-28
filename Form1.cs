496323678fde0cec1734379993bcf2591bb308da
2274801030138 - VŨ MINH THẮNG - 71K28KTPM03 <thang.2274801030138@vanlanguni.vn>
2023-11-29 00:25:05 +0700

    update

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,51 @@ namespace DoAn_CuoiKy
 		{
 			InitializeComponent();
 		}
+
+		private void btnDangNhap_Click(object sender, EventArgs e)
+		{
+			SqlConnection loginCon = new SqlConnection("Data Source=LAPTOP-7H1PG1MJ\\MSSQLSERVER01;Initial Catalog=QuanLyThiTracNgiem;Integrated Security=True");
+			SqlCommand cmd = new SqlCommand("select * from _User where TenDN = '"+txtTaiKhoan.Text+"' and MK = '"+txtMatKhau.Text+"'",loginCon);
+			SqlDataAdapter sda = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			sda.Fill(dt);
+			string cmbItemValue = comboBox1.SelectedItem.ToString();
+
+			if (dt.Rows.Count > 0 )
+			{
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					if (dt.Rows[i]["UserRoles"].ToString() == cmbItemValue)
+					{
+						MessageBox.Show("you are login as" + dt.Rows[i][2]);
+						if(comboBox1.SelectedIndex==0)
+						{
+							FormAdmin admin = new FormAdmin();
+							admin.Show();
+							this.Hide();
+						}
+						else if (comboBox1.SelectedIndex==1)
+						{
+							FormStudent student = new FormStudent();
+							student.Show();
+							this.Hide();
+						}
+						else
+						{
+							FormTeacher formTeacher = new FormTeacher();
+							formTeacher.Show();
+							this.Hide();
+						}
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("Error");
+			}
+
+		}
+
+
 	}
 }
